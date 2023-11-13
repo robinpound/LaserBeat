b0400b58026f2629d932845e7280988353e28751
Michael Hayes <115077851+m-hayes95@users.noreply.github.com>
2023-11-13 17:09:32 +0000

    Revert "Move from prototype to Alpha build"
    This reverts commit f04fcd063bb46716594eafb29fa1cd2c24306f74.

--- Assets/Systems/Targets/Target1Tag.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-public class Target1Tag : MonoBehaviour
-{
-    // This class is used as a Tag to reference the attached game object
-}
--- Assets/Systems/Targets/Target2Tag.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-public class Target2Tag : MonoBehaviour
-{
-    // This class is used as a Tag to reference the attached game object
-}
