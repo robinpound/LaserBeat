129f19dd4376737c108212fc62413776271358f1
Robin Pound <54936470+robinpound@users.noreply.github.com>
2023-11-13 20:26:14 +0000

    Files Created & Spawner.cs created & Prefabs created

---
+++ Assets/Systems/Targets/TarSpawSys/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+    [Header("Settings")]
+    [Tooltip("SpawnRate is the number of targets spawning per second (Targets/s)")]
+
+    [Range(0, 5)][SerializeField] float SpawnRate;
+    [Header("References")]
+    [SerializeField] GameObject Target;
+
+    public void StartSpawning()
+    {
+        StartCoroutine(SpawnOnLoop());
+    }
+    private IEnumerator SpawnOnLoop()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(SpawnRate);
+            spawnRandomTarget();
+        }
+    }
+    private void spawnRandomTarget()
+    {
+    }
+}
