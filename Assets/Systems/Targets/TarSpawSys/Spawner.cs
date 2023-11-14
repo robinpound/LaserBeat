25aed18b239922a94f73f3cc426c29716135d3bd
Robin Pound <54936470+robinpound@users.noreply.github.com>
2023-11-14 15:42:53 +0000

    Spawner.cs & main scene edit

--- Assets/Systems/Targets/TarSpawSys/Spawner.cs
+++ Assets/Systems/Targets/TarSpawSys/Spawner.cs
@@ -1,30 +1,48 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using static Spawner;
 using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour
 {
-    [Header("Settings")]
-    [Tooltip("SpawnRate is the number of targets spawning per second (Targets/s)")]
-
-    [Range(0, 5)][SerializeField] float SpawnRate;
-    [Header("References")]
-    [SerializeField] GameObject Target;
-
-    public void StartSpawning()
+    [System.Serializable]
+    public class SpawnData 
     {
-        StartCoroutine(SpawnOnLoop());
+        public GameObject Target;
+        [Range(0, 100)] public int size;
+        [Range(0, 5)] public float SpawnRate;
     }
+
+    public SpawnData spawnData;
+    public Queue<GameObject> pool;
+
+    // FILL POOL
+    public void Start() 
+    {
+        spawnData = new SpawnData();
+        pool = new Queue<GameObject>();
+        
+        for (int i = 0; i < pool.Count; i++) 
+        {
+            GameObject tar = Instantiate(spawnData.Target);
+            tar.SetActive(true); //set to false
+            pool.Enqueue(tar);
+        }
+    }
+    // SPAWNING 
+    public void StartSpawningSequence() => StartCoroutine(SpawnOnLoop());
     private IEnumerator SpawnOnLoop()
     {
         while (true)
         {
-            yield return new WaitForSeconds(SpawnRate);
+            yield return new WaitForSeconds(spawnData.SpawnRate);
             spawnRandomTarget();
         }
     }
     private void spawnRandomTarget()
     {
+        
     }
 }
