4d8e89e782a372b57d9f5dab95364d580c15c003
Michael Hayes <115077851+m-hayes95@users.noreply.github.com>
2023-11-16 00:12:51 +0000

    Mike test scene & player input script

---
+++ Assets/Systems/Laser Guns/PlayerInput.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Liminal.SDK.VR;
+using Liminal.SDK.VR.Input;
+
+
+public class PlayerInput : MonoBehaviour
+{
+    private void Update() 
+    {
+        var rightHandInput = GetInput(VRInputDeviceHand.Right);
+        var leftHandInput = GetInput(VRInputDeviceHand.Left);
+
+        if (rightHandInput != null)
+        {
+            if (rightHandInput.GetButtonDown(VRButton.One))
+            {
+                Debug.Log("Call right hand input");
+            }
+        }
+
+        if (leftHandInput != null)
+        {
+            if (leftHandInput.GetButtonDown(VRButton.One))
+            {
+                Debug.Log("Call left hand input");
+            }
+        }
+    }
+
+    private IVRInputDevice GetInput(VRInputDeviceHand hand)
+    {
+        var device = VRDevice.Device;
+        return hand == VRInputDeviceHand.Left ? device.SecondaryInputDevice : device.PrimaryInputDevice;
+    }
+
+}
