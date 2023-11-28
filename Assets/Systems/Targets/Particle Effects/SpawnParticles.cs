7afba5f20c5bef32912d8362ce81349259b5d7f1
Michael Hayes <115077851+m-hayes95@users.noreply.github.com>
2023-11-28 20:24:06 +0000

    Added particle spawn logic on hit
    + using on laser hit event

---
+++ Assets/Systems/Targets/Particle Effects/SpawnParticles.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SpawnParticles : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem particleSystem;
+
+    void OnEnable() => LaserGunRayCast.OnLaserHit += AcitvateParticleEffect;
+    void OnDisable() => LaserGunRayCast.OnLaserHit -= AcitvateParticleEffect;
+
+    private void AcitvateParticleEffect(RaycastHit h)
+    {
+        Instantiate(particleSystem, h.point, Quaternion.identity);  
+    }
+}
