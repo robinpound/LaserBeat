0e809a529b9f9b94efc9c56ffe4396acabb7b5c4
Robin Pound <54936470+robinpound@users.noreply.github.com>
2023-12-04 12:21:08 +0000

    Spinning Target

---
+++ Assets/Systems/Targets/Target/SpinTarget.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpinTarget : MonoBehaviour
+{
+    public float rotationSpeed = 10f;
+
+    void start() 
+    {
+        transform.Rotate(Vector3.forward, Random.Range(0, 360));
+    }
+    void Update()
+    {
+        transform.Rotate(new Vector3(0.5f,0.5f,1), rotationSpeed * Time.deltaTime);
+
+    }
+}
